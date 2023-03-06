cd8e261512412144f94e276381ef4d61235fe0c3
ranjithkiran123 <92369725+ranjithkiran123@users.noreply.github.com>
2023-03-06 22:31:41 +0530

    Add files via upload

---
+++ Number of Students Doing Homework at a Given Time.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def busyStudent(self, startTime, endTime, queryTime):
+      
+        num = 0
+        for i in range(len(startTime)):
+            if startTime[i]<= queryTime and queryTime<=endTime[i]:
+                num += 1
+        return num
+
